8e6c3683fdbe16a0c8147644b0ff16df01fc9184
Paweł Kata <gmabber@gmail.com>
2019-05-15 23:43:26 +0200

    Fixes for new MKM url’s.

--- utils/utils.js
+++ utils/utils.js
@@ -5,43 +5,46 @@ const Crypto = require('crypto');
 var utils = {};
 utils.debug = false;
 
+const MKM_API_URL = 'api.cardmarket.com'
+const MKM_SANDBOX_URL = 'sandbox.cardmarket.com'
+
 // keys
-	// !app_key
-	// !secret
-	// access_token_secret
-	// access_token
-utils.get = function(keys, path, data, headers, tokens){
+// !app_key
+// !secret
+// access_token_secret
+// access_token
+utils.get = function (keys, path, data, headers, tokens) {
 	return utils.__mkmrequest(keys, 'GET', path, data, headers);
 };
 
-utils.post = function(keys, path, data, headers, tokens){
+utils.post = function (keys, path, data, headers, tokens) {
 	return utils.__mkmrequest(keys, 'POST', path, data, headers);
 };
 
-utils.__mkmrequest = function(keys, method, path, data, headers, tokens){
+utils.__mkmrequest = function (keys, method, path, data, headers, tokens) {
 
-	if(data){
-		for(var k in data){
-			if(typeof data[k] === 'string'){
+	if (data) {
+		for (var k in data) {
+			if (typeof data[k] === 'string') {
 				data[k] = data[k].replace(/\'/g, ' ');
 			}
 		}
 	}
 
-	if(tokens){
+	if (tokens) {
 		keys.access_token = tokens.access_token;
 		keys.access_token_secret = access_token_secret;
 	}
 
 	var oauth_header = getOauthHeader(method, path, {
-		oauth_version : '1.0',
+		oauth_version: '1.0',
 		oauth_timestamp: Math.round(Date.now() / 1000),
 		oauth_nonce: generateNonce(),
 		oauth_signature_method: 'HMAC-SHA1',
 		oauth_consumer_key: keys.app_key,
 		oauth_token: keys.access_token || ''
 	}, data, keys);
-	if(!headers) {
+	if (!headers) {
 		headers = {};
 	}
 	headers['Authorization'] = oauth_header;
@@ -49,25 +52,25 @@ utils.__mkmrequest = function(keys, method, path, data, headers, tokens){
 	var params = {
 		https: true,
 		method: method,
-		host: utils.debug ? 'sandbox.mkmapi.eu' : 'www.mkmapi.eu',
+		host: utils.debug ? MKM_SANDBOX_URL : MKM_API_URL,
 		path: path,
-		headers:headers
+		headers: headers
 	};
-	if(data){
+	if (data) {
 		params.data = data;
 	}
 
-	if(utils.debug){
+	if (utils.debug) {
 		console.log('[MKM-API] Request', params);
 	}
 
 	return fishingrod.fish(params);
 };
 
-function getOauthHeader(method, path, params, data, keys){
+function getOauthHeader(method, path, params, data, keys) {
 	var header = `OAuth realm="${getUri(path)}",`;
-	for(var k in params){
-		header += k + '="' + params[k] +'",';
+	for (var k in params) {
+		header += k + '="' + params[k] + '",';
 	}
 
 	var signature = getSignature(method, path, params, data, keys);
@@ -76,11 +79,11 @@ function getOauthHeader(method, path, params, data, keys){
 }
 utils.getOauthHeader = getOauthHeader;
 
-function getUri(path){
-	return 'https://' + (utils.debug ? 'sandbox.mkmapi.eu' : 'www.mkmapi.eu' ) + path;
+function getUri(path) {
+	return 'https://' + (utils.debug ? MKM_SANDBOX_URL : MKM_API_URL) + path;
 }
 
-function getSignature(method, path, params, data, keys){
+function getSignature(method, path, params, data, keys) {
 	var str = getFinalString(method, path, params, data);
 	var signing_key = getSigningKey(keys);
 
@@ -91,7 +94,7 @@ function getSignature(method, path, params, data, keys){
 }
 utils.getSignature = getSignature;
 
-function getFinalString(method, path, params, data){
+function getFinalString(method, path, params, data) {
 	var uri = getUri(path);
 	var str = method + '&' + percentencode(uri) + '&';
 	str += percentencode(buildParams(params, data));
@@ -99,15 +102,15 @@ function getFinalString(method, path, params, data){
 }
 utils.getFinalString = getFinalString;
 
-function getSigningKey(keys){
+function getSigningKey(keys) {
 	return percentencode(keys.secret) + '&' + (keys.access_token_secret ? percentencode(keys.access_token_secret) : '');
 }
 utils.getSigningKey = getSigningKey;
 
-function buildParams(params, data){
+function buildParams(params, data) {
 	params = makeParams(params, data);
 	var str = [];
-	for(var k in params){
+	for (var k in params) {
 		str.push(k + '=' + encodeURIComponent(params[k]));
 	}
 	str = str.join('&');
@@ -115,36 +118,36 @@ function buildParams(params, data){
 }
 utils.buildParams = buildParams;
 
-function makeParams(params, data){
+function makeParams(params, data) {
 	const needed_params = ['version', 'timestamp', 'nonce', 'consumer_key', 'token', 'signature_method'];
-	for(var k in data){
+	for (var k in data) {
 		params[k] = data[k];
 	}
 	params = sortObject(params);
-	for(var k of needed_params){
-		if(!(('oauth_' + k) in params)){
+	for (var k of needed_params) {
+		if (!(('oauth_' + k) in params)) {
 			params[('oauth_' + k)] = '';
 		}
 	}
 	return params;
 }
 
-function sortObject(obj){
+function sortObject(obj) {
 	var sorted = Object.keys(obj);
-	sorted.sort(function(a, b){
-		if(a > b) return 1;
-		if(a < b) return -1;
+	sorted.sort(function (a, b) {
+		if (a > b) return 1;
+		if (a < b) return -1;
 		return 0;
 	});
 	var ret = {};
-	for(var k of sorted){
+	for (var k of sorted) {
 		ret[k] = obj[k];
 	}
 	return ret;
 }
 
-function generateNonce(){
-	function s4(){
+function generateNonce() {
+	function s4() {
 		return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 	}
 
